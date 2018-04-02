4d28d2724e76d53ee25d87935c5f64848cdbd5c8
Antonio Alex Marques Albuquerque <alex.marques@textiluniao.com.br>
2018-04-02 19:31:50 -0300

    Initial commit

---
+++ src/main/java/com/alexmarques/firstspringboot/controller/FirstSpringBootController.java
@@ -0,0 +1,18 @@
+package com.alexmarques.firstspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstSpringBootController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Index page";
+	}	
+	
+	@RequestMapping("/showtext")
+	public String showText() {
+		return "Hello, first Spring boot project";
+	}
+}
